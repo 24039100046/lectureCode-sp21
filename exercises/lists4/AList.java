b971760dd7b6ba034144e3cb66fe6b34bca3cba1
24039100046 <24039100046@stu.xidian.edu.cn>
2026-06-01 13:29:35 +0800

    Update AList.java
    List4 Lecture DIY

--- exercises/lists4/AList.java
+++ exercises/lists4/AList.java
@@ -3,31 +3,64 @@
  */
 
 public class AList {
+    private int[] list;
+    private int size;
     /** Creates an empty list. */
     public AList() {
+        list = new int[1];
+        size = 0;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        if (size == list.length) {
+            resizeList(list.length * 2);
+        }
+        list[size] = x;
+        size++;
+    }
+
+    /**
+     * Resizes the list.
+     */
+    private void resizeList(int n) {
+        int[] newList = new int[n];
+        System.arraycopy(list, 0, newList, 0, size);
+        list = newList;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        if (size != 0) {
+            return list[size - 1];
+        }
+        return 0;
     }
+
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return list[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        //int temp = list[size];
+        int temp = getLast();
+        //data abstraction
+        list[size] = 0;
+        //There is no need to destroy it when it is basic type.
+        //While it is good to destroy it when it is reference type.
+        size--;
+        if (size < list.length / 4) {
+            //usage ratio 25%
+            resizeList(list.length / 2);
+        }
+        return temp;
     }
 } 
